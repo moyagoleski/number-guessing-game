76e94b9a4c51a597a4a4f313694c6ea0b0d06656
moyagoleski <moyagoleski@gmail.com>
2022-01-07 21:12:42 -0500

    Variables created in main.js
    - variables created for enter button, output text, number user has to guess, and number user submitted

--- js/main.js
+++ js/main.js
@@ -4,3 +4,10 @@
   Last Updated: 01/07/2022
   Source: https://www.youtube.com/watch?v=2cQUkYU8AmI
 */
+// VARIABLES
+let enterBtn = document.getElementById('enterBtn');
+let output = document.getElementById('outputText');
+// stores rounded/random number user has to guess
+let number = [Math.floor(Math.random() * 100)];
+// stores value user submitted
+let input = document.getElementById('userInput').value;
