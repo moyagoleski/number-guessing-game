041717c029a9a4b6816176f000263148b4fdf400
moyagoleski <moyagoleski@gmail.com>
2022-01-07 21:35:49 -0500

    Event Listener added main.js
    - event listener added for enter btn
    - if else structure if user enter the right number, if the number was too low, or if the number was too high
    - typo fixed in index.html for script

--- js/main.js
+++ js/main.js
@@ -9,5 +9,26 @@ let enterBtn = document.getElementById('enterBtn');
 let output = document.getElementById('outputText');
 // stores rounded/random number user has to guess
 let number = [Math.floor(Math.random() * 100)];
-// stores value user submitted
-let input = document.getElementById('userInput').value;
+
+// EVENT LISTENER
+enterBtn.addEventListener('click', function(){
+  // stores value user submitted
+  let input = document.getElementById('userInput').value;
+  // console.log("button has been clicked " + number);
+  // if input matches number
+  if (input == number){
+    // console.log("You guessed right, your number was " + number);
+    output.innerHTML = `You guessed right, the number was ${number}!`;
+  }
+  // if input is lower than number
+  else if (input < number){
+    // console.log("You guessed too low!");
+    output.innerHTML = "You guessed too low, please try again!"
+  }
+  // if inout is higher than number
+  else {
+    // console.log("You guessed too high!");
+    output.innerHTML = "You guessed too high, please try again!"
+  }
+  
+});
