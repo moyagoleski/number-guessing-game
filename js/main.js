443438f301d792525e9998fd02fb324009a72aec
moyagoleski <moyagoleski@gmail.com>
2022-01-08 22:50:04 -0500

    Updates to main.js
    - if/else structure added to determine if the user entered a number or not (validation)
    - minor bug fixed in the number variable (removed it from an array)

--- js/main.js
+++ js/main.js
@@ -8,27 +8,40 @@
 let enterBtn = document.getElementById('enterBtn');
 let output = document.getElementById('outputText');
 // stores rounded/random number user has to guess
-let number = [Math.floor(Math.random() * 100)];
+let number = Math.floor(Math.random() * 100);
+
+// testing purposes - hide later
+console.log(number);
 
 // EVENT LISTENER
+// when the user clicks the enter btn
 enterBtn.addEventListener('click', function(){
   // stores value user submitted
   let input = document.getElementById('userInput').value;
-  // console.log("button has been clicked " + number);
-  // if input matches number
-  if (input == number){
-    // console.log("You guessed right, your number was " + number);
-    output.innerHTML = `You guessed right, the number was ${number}!`;
+
+  // if user input is not a number
+  if (isNaN(input)) {
+    // console.log("not number");
+    output.innerHTML = "Invalid Entry - Please enter a number";
   }
-  // if input is lower than number
-  else if (input < number){
-    // console.log("You guessed too low!");
-    output.innerHTML = "You guessed too low, please try again!"
-  }
-  // if inout is higher than number
+  // if user input is a number
   else {
-    // console.log("You guessed too high!");
-    output.innerHTML = "You guessed too high, please try again!"
+    // console.log("a number");
+    // if input matches number
+    if (input == number){
+      // console.log("You guessed right, your number was " + number);
+      output.innerHTML = `You guessed right, the number was ${number}!`;
+    }
+    // if input is lower than number
+    else if (input < number){
+      // console.log("You guessed too low!");
+      output.innerHTML = "You guessed too low, please try again!"
+    }
+    // if input is higher than number
+    else {
+      // console.log("You guessed too high!");
+      output.innerHTML = "You guessed too high, please try again!"
+    }
   }
-  
+
 });
